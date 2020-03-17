1ac8e753a0d702506c853bd2685ed41e7bf28c2e
Rafael Briceño <rafaelbm9126@gmail.com>
2020-03-16 23:22:04 -0500

    fix: repair find all

--- admin/api/post/services/Post.js
+++ admin/api/post/services/Post.js
@@ -3,13 +3,7 @@
 module.exports = {
 
   fetchAll: (params) => {
-    const convertedParams = strapi.utils.models.convertParams('post', params);
-    return strapi.query('post').find()
-      // .where(convertedParams.where)
-      // .sort(convertedParams.sort)
-      // .skip(convertedParams.start)
-      .limit(convertedParams.limit);
-    // .populate(_.keys(_.groupBy(_.reject(strapi.models.post.associations, {autoPopulate: false}), 'alias')).join(' '));
+    return strapi.query('post').find({ _limit: params._limit });
   },
 
   fetchOne: (params) => {
